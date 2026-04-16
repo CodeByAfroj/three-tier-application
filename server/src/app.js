2e8868221a6d9ce0f74ee342739c975e4cd6ffda
CodeByAfroj <mulaniafroj26@gmail.com>
2026-04-16 18:26:33 +0530

    test

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ dotenv.config()
 connectDB();
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'API is working and running...' });
