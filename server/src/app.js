1e47bf3806c681060c090facfa9f249350da428e
CodeByAfroj <mulaniafroj26@gmail.com>
2026-04-16 18:50:26 +0530

    test

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'API is working and running...' });
+  res.json({ message: 'API is working and running...' })
 });
 
 app.get("/health", (req, res) => {
