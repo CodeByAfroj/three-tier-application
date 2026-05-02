ce197c34c54f339ce3cda017e2033642806cc44a
Afroj Mulani <mulaniafroj26@gmail.com>
2026-05-02 10:03:38 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
+import contentRoutes from './routes/content.js'
 
 dotenv.config();
 connectDB();
